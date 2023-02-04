ecb20349b35232f4479793170ff39873a32c3e9d
Kim Rutherford <kmr@aska.gen.nz>
2023-02-04 21:18:59 +1300

    Add a view for PDB structures
    Refs pombase/website#1997

--- structure_view/views.py
+++ structure_view/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     protein_id = request.GET.get('protein_id', '').strip()
+    structure_type = request.GET.get('structure_type', '').strip()
 
     context = {
         'protein_id': protein_id
     }
-    return render(request, 'structure_view/index.html', context)
-
+    if structure_type == 'alphafold':
+        return render(request, 'structure_view/index.html', context)
+    else:
+        return render(request, 'structure_view/pdb_index.html', context)
