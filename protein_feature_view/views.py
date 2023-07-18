aa292aabd53f412319684db1279011a06193c094
Kim Rutherford <kmr@aska.gen.nz>
2023-07-18 21:24:58 +1200

    Pass full_or_widget to template
    Refs pombase/website#2071

--- protein_feature_view/views.py
+++ protein_feature_view/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+    full_or_widget = request.GET.get('full_or_widget', '').strip()
     gene_uniquename = request.GET.get('gene_uniquename', '').strip()
 
     context = {
+        'full_or_widget': full_or_widget,
         'gene_uniquename': gene_uniquename
     }
 
