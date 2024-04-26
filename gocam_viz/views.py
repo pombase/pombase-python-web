22be7843194ca5300ce9e9193c756b3e8f3f8f59
Kim Rutherford <kmr@aska.gen.nz>
2024-04-26 23:33:18 +1200

    Add basic style for GO-CAM widget
    Refs pombase/website#2169

--- gocam_viz/views.py
+++ gocam_viz/views.py
@@ -8,7 +8,7 @@ def index(request):
     if len(gocam_ids) == 0:
         gocam_viz_html = f"No GO-CAM pathway for {gene_uniquename}";
     else:
-        gocam_viz_html = f"<wc-gocam-viz id='gocam-1' api-url='https://api.geneontology.xyz/gocam/%ID/raw' gocam-id='{gocam_ids[0]}'></wc-gocam-viz>";
+        gocam_viz_html = f"<wc-gocam-viz style='--button-background: #2598c5; --button-background-hover: #2598c5; -- --button-color: #fff; --panel-header-border-width: 1px; --panel-header-border-color: #ddd; --border-color: #d1c4e9; --panel-header-padding: 0.2em; --panel-header-background: #f2f2f2; --graph-height: 420px;' id='gocam-1' api-url='https://api.geneontology.xyz/gocam/%ID/raw' gocam-id='{gocam_ids[0]}'></wc-gocam-viz>";
 
     context = {
 #        'full_or_widget': full_or_widget,
