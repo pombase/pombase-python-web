3c30674db5d7a13ea72fd389dfbdd8dff87fb08d
Kim Rutherford <kmr@aska.gen.nz>
2024-04-30 19:42:56 +1200

    Add a full page gocam_viz option
    Refs pombase/website#2169

--- gocam_viz/views.py
+++ gocam_viz/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 def index(request):
     full_or_widget = request.GET.get('full_or_widget', '').strip()
-    gene_uniquename = request.GET.get('gene_uniquename', '').strip()
-    gocam_ids = request.GET.get('gocam_ids', '').strip().split(',')
+    gocam_id = request.GET.get('gocam_id', '').strip()
 
-    if len(gocam_ids) == 0:
-        gocam_viz_html = f"No GO-CAM pathway for {gene_uniquename}";
+    if full_or_widget == 'widget':
+        graph_height = 420
     else:
-        gocam_viz_html = f"<wc-gocam-viz style='--button-background: #2598c5; --button-background-hover: #2598c5; -- --button-color: #fff; --panel-header-border-width: 1px; --panel-header-border-color: #ddd; --border-color: #d1c4e9; --panel-header-padding: 0.2em; --panel-header-background: #f2f2f2; --graph-height: 420px;' id='gocam-1' api-url='https://api.geneontology.xyz/gocam/%ID/raw' gocam-id='{gocam_ids[0]}'></wc-gocam-viz>";
+        graph_height = 650
+
+    gocam_viz_html = f"<wc-gocam-viz style='--button-background: #2598c5; --button-background-hover: #2598c5; -- --button-color: #fff; --panel-header-border-width: 1px; --panel-header-border-color: #ddd; --border-color: #d1c4e9; --panel-header-padding: 0.2em; --panel-header-background: #f2f2f2; --graph-height: {graph_height}px;' id='gocam-1' api-url='https://api.geneontology.xyz/gocam/%ID/raw' gocam-id='{gocam_id}'></wc-gocam-viz>";
+
+    print(gocam_viz_html)
 
     context = {
 #        'full_or_widget': full_or_widget,
