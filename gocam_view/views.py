71314706c2efb13e5a4a5b8ed695fd1a82d129cb
Kim Rutherford <kmr@aska.gen.nz>
2025-03-09 01:33:24 +1300

    Add highlight_gene_ids options
    Allows highlight activities by gene ID.
    
    Refs pombase/website#2346

--- gocam_view/views.py
+++ gocam_view/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 def index(request):
     full_or_widget = request.GET.get('full_or_widget', '').strip()
     gocam_id = request.GET.get('gocam_id', '').strip()
+    highlight_gene_ids = request.GET.get('highlight_gene_ids', '').strip()
 
     if full_or_widget == 'widget':
         graph_height = 420
@@ -12,6 +13,7 @@ def index(request):
     context = {
         'full_or_widget': full_or_widget,
         'gocam_id': gocam_id,
+        'highlight_gene_ids': highlight_gene_ids.split(','),
     }
 
     return render(request, 'gocam_view/index.html', context)
