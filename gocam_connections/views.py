2a4b322d830331b7bfb4bfc65935ab2e804cc4e1
Kim Rutherford <kmr@aska.gen.nz>
2025-05-22 22:35:03 +1200

    Add "summary_type" argument for gocam_connections
    Refs pombase/pombase-gocam#30

--- gocam_connections/views.py
+++ gocam_connections/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
+    summary_type = request.GET.get('summary_type', 'connected_only').strip()
+
     context = {
+        'summary_type': summary_type,
     }
 
     return render(request, 'gocam_connections/index.html', context)
