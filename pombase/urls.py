36ab02a8af4423448c81cceac7ad4d4fd49adb1b
Kim Rutherford <kmr@aska.gen.nz>
2025-09-20 01:15:54 +1200

    Disable non-gocam apps if there's no config

--- pombase/urls.py
+++ pombase/urls.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
 
+import os
+
 urlpatterns = [
-    path('motifsearch/', include('motifsearch.urls')),
-    path('gene_ex/', include('gene_ex.urls')),
-    path('structure_view/', include('structure_view.urls')),
-    path('rna_2d_structure/', include('rna_2d_structure.urls')),
-    path('protein_feature_view/', include('protein_feature_view.urls')),
     path('gocam_viz/', include('gocam_viz.urls')),
     path('gocam_view/', include('gocam_view.urls')),
     path('gocam_connections/', include('gocam_connections.urls')),
-    path('rhea_widget/', include('rhea_widget.urls')),
-    path('stats/', include('stats.urls')),
-    path('admin/', admin.site.urls),
 ]
+
+if len(os.environ["WEBSITE_CONFIG_JSON_PATH"]) > 0:
+    urlpatterns.append(path('motifsearch/', include('motifsearch.urls')))
+    urlpatterns.append(path('gene_ex/', include('gene_ex.urls')))
+    urlpatterns.append(path('structure_view/', include('structure_view.urls')))
+    urlpatterns.append(path('rna_2d_structure/', include('rna_2d_structure.urls')))
+    urlpatterns.append(path('protein_feature_view/', include('protein_feature_view.urls')))
+    urlpatterns.append(path('rhea_widget/', include('rhea_widget.urls')))
+    urlpatterns.append(path('stats/', include('stats.urls')))
+    urlpatterns.append(path('admin/', admin.site.urls))
