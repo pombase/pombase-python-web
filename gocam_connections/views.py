5536af4aa6c472be0de5a8b14f8bc52a0afdd6cd
Kim Rutherford <kmr@aska.gen.nz>
2025-09-19 20:47:03 +1200

    Fix "View joined models" button
    After FlyBase changes.
    
    Refs pombase/pombase-gocam#105

--- gocam_connections/views.py
+++ gocam_connections/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 def index(request):
     summary_type = request.GET.get('summary_type', 'connected_only').strip()
     api_path = request.GET.get('api_path', '').strip()
+    model_view_url = request.GET.get('model_view_url', '').strip()
 
     context = {
         'summary_type': summary_type,
         'api_path': api_path,
+        'model_view_url': model_view_url
     }
 
     return render(request, 'gocam_connections/index.html', context)
