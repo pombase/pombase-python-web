0cb8f038ea8ac360f82591e5740695cd55d0f8c8
Kim Rutherford <kmr@aska.gen.nz>
2025-11-27 21:34:32 +1300

    Add an optional flags param for gocam_connections
    Refs pombase/pombase-gocam#107

--- gocam_connections/views.py
+++ gocam_connections/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 def index(request):
     summary_type = request.GET.get('summary_type', 'connected_only').strip()
     api_path = request.GET.get('api_path', '').strip()
+    flags = request.GET.get('flags', '').strip()
     model_view_path = request.GET.get('model_view_path', '').strip()
 
     context = {
         'summary_type': summary_type,
         'api_path': api_path,
+        'flags': flags,
         'model_view_path': model_view_path
     }
 
